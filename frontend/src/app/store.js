eeeee00895390574c373e2ff362fb9e2a847a612
bilalfareed83 <74311496+cssrgit@users.noreply.github.com>
2022-05-07 12:02:14 +0500

    update register and logout functions

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducers from "../features/auth/authSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    auth: authReducers,
+  },
 });
